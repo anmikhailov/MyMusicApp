17f90a400af5c2e7c05db41c88856ea32fbb76fa
AndreyMikhailov <mands315@gmail.com>
2023-06-12 16:15:54 +0300

    Rename some files and create files in empty folders

---
+++ MyMusicApp/Model/UserModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserModel.swift
+//  MyMusicApp
+//
+//  Created by Andrey on 12.06.2023.
+//
+
+import Foundation
---
+++ MyMusicApp/Services/FirebaseManager.swift
@@ -0,0 +1,8 @@
+//
+//  FirebaseManager.swift
+//  MyMusicApp
+//
+//  Created by Andrey on 12.06.2023.
+//
+
+import Foundation
