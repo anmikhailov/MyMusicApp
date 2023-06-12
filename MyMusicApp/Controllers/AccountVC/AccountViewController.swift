f5432788bd0047bfa7a97c64b174c7beab30ae7e
Privetyanikita <72889842+Privetyanikita@users.noreply.github.com>
2023-06-12 21:18:08 +0200

    add AccountView

--- MyMusicApp/Controllers/AccountVC/AccountViewController.swift
+++ MyMusicApp/Controllers/AccountVC/AccountViewController.swift
@@ -6,11 +6,50 @@
 //
 
 import UIKit
+import SnapKit
 
 class AccountViewController: UIViewController {
+    
+    var accountLabel = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        SetBacgroundColors()
 
     }
+    
+    func SetBacgroundColors(){
+        view.backgroundColor = Resources.Colors.TabBarColors.background
+    }
+    
+    func setProfileLabel(){
+        accountLabel.text = "Account"
+        
+    }
+    
 }
+
+
+#if DEBUG
+
+import SwiftUI
+
+struct AccountViewControllerRepresentable: UIViewControllerRepresentable {
+    typealias UIViewControllerType = AccountViewController
+
+    func makeUIViewController(context: Context) -> UIViewControllerType {
+        AccountViewController(nibName: nil, bundle: nil)
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        
+    }
+}
+
+struct AccountViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        AccountViewControllerRepresentable()
+    }
+}
+
+#endif
