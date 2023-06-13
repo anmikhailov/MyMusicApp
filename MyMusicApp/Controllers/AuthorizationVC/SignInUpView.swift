e1216647d5f0a6ac1d733b8a6257efeb90798885
AndreyMikhailov <mands315@gmail.com>
2023-06-13 16:13:39 +0300

    Return google icon button

--- MyMusicApp/Controllers/AuthorizationVC/SignInUpView.swift
+++ MyMusicApp/Controllers/AuthorizationVC/SignInUpView.swift
@@ -173,9 +173,9 @@ class SignInUpView: CustomView {
         return element
     }()
     
-    private lazy var googleAuthButton: GIDSignInButton = {
-        let element = GIDSignInButton()
-        element.style = .iconOnly
+    private lazy var googleAuthButton: UIButton = {
+        let element = UIButton()
+        element.setBackgroundImage(Resources.Icons.SignInUp.googleIcon, for: .normal)
         element.translatesAutoresizingMaskIntoConstraints = false
         element.addTarget(self, action: #selector(didTapGoogleAuthButton(_:)), for: .touchUpInside)
         return element
