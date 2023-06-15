0a793ddd05ab6552750f019faad29389a2c61cb5
AndreyMikhailov <mands315@gmail.com>
2023-06-15 17:07:10 +0300

    Developing Search Screen

--- MyMusicApp/Controllers/SeachVC/SearchView.swift
+++ MyMusicApp/Controllers/SeachVC/SearchView.swift
@@ -8,34 +8,115 @@
 import UIKit
 
 protocol SearchViewDelegate: AnyObject {
-    func searchView(_ view: SearchView, didTapButton button: UIButton)
+    func searchView(_ view: SearchView, didTapCloseButton button: UIButton)
 }
 
-class SearchView: CustomView {
+class SearchView: CustomView, UISearchResultsUpdating {
+    
     weak var delegate: SearchViewDelegate?
     
     //MARK: - Variables
-    // Add private lazy variables here
+    private lazy var topHStackView: UIStackView = {
+        let element = UIStackView()
+        element.axis = .horizontal
+        element.distribution = .fill
+        element.spacing = 10
+        element.alignment = .center
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    lazy var searchView: UISearchBar = {
+        let element = UISearchBar()
+        element.searchBarStyle = .minimal
+        element.placeholder = "Search"
+        element.backgroundColor = Resources.Colors.neutral1.withAlphaComponent(0.7)
+        element.layer.cornerRadius = 10
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var closeWindowButton: UIButton = {
+        let element = UIButton()
+        element.setTitle("Cancel", for: .normal)
+        element.setTitleColor(Resources.Colors.brand1, for: .normal)
+        element.titleLabel?.font = UIFont.systemFont(ofSize: 14)
+        element.translatesAutoresizingMaskIntoConstraints = false
+        element.addTarget(self, action: #selector(didTapCloseButton(_:)), for: .touchUpInside)
+        return element
+    }()
+    
+//    private lazy var topSegmentControl: CustomSegmentedControl = {
+//        let element = CustomSegmentedControl(frame: CGRect(x: 0, y: 100, width: 380, height: 50), buttonTitle: ["All", "Artist", "Album", "Song", "Playlist"])
+//        element.selectorViewColor = .white
+//        element.selectorTextColor = .white
+//        element.backgroundColor = .clear
+//        element.translatesAutoresizingMaskIntoConstraints = false
+//        return element
+//    }()
+    
+    lazy var searchItemsCollectionView: UICollectionView = {
+        let element = UICollectionView()
+        
+        return element
+    }()
+    
+    lazy var mainTableView: UITableView = {
+        let element = UITableView()
+        element.register(SearchCell.self, forCellReuseIdentifier: SearchCell.identifier)
+        element.backgroundColor = .clear
+        element.separatorStyle = .none
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
     
     //MARK: - setViews
     override func setViews() {
         super.setViews()
+        self.backgroundColor = Resources.Colors.brand2
         
-        // Add subviews here
+        self.addSubview(topHStackView)
+        
+        topHStackView.addArrangedSubview(searchView)
+        topHStackView.addArrangedSubview(closeWindowButton)
+        
+//        self.addSubview(topSegmentControl)
+        self.addSubview(mainTableView)
     }
     
     //MARK: - layoutViews
     override func layoutViews() {
         super.layoutViews()
         
-        // Add constraints here
+        NSLayoutConstraint.activate([
+//            topSegmentControl.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 10),
+//            topSegmentControl.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10),
+//            topSegmentControl.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -10),
+//            topSegmentControl.heightAnchor.constraint(equalToConstant: 60),
+            
+            topHStackView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 20),
+            topHStackView.heightAnchor.constraint(equalToConstant: 40),
+            topHStackView.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor, constant: 30),
+            topHStackView.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor, constant: -30),
+            
+            searchView.widthAnchor.constraint(greaterThanOrEqualToConstant: 300),
+            
+            mainTableView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 100),
+            mainTableView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10),
+            mainTableView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -10),
+            mainTableView.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor, constant: -10),
+        ])
+    }
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        
     }
 }
 
 //MARK: - Target Actions
 private extension SearchView {
-    @objc func didTapButton(_ button: UIButton) {
-        delegate?.searchView(self, didTapButton: button)
+    @objc func didTapCloseButton(_ button: UIButton) {
+        delegate?.searchView(self, didTapCloseButton: button)
     }
 }
 
--- MyMusicApp/Controllers/SeachVC/SearchViewController.swift
+++ MyMusicApp/Controllers/SeachVC/SearchViewController.swift
@@ -9,15 +9,98 @@ import UIKit
 
 class SearchViewController: CustomViewController<SearchView> {
     
+    let sectionsTitles = ["Top Searching", "Artist", "Album", "Song", "Playlist"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         customView.delegate = self
+        
+        customView.mainTableView.dataSource = self
+        customView.mainTableView.delegate = self
+        
+        customView.searchItemsCollectionView.dataSource = self
+        customView.searchItemsCollectionView.delegate = self
+    }
+    
+    @objc func addTapped() {
+        
     }
 }
 
 //MARK: - SearchViewDelegate
 extension SearchViewController: SearchViewDelegate {
-    func searchView(_ view: SearchView, didTapButton button: UIButton) {
+    func searchView(_ view: SearchView, didTapCloseButton button: UIButton) {
+        //Close SearchViewController
+        print("Close button tapped")
     }
+}
+
+//MARK: - UISearchResultsUpdating
+extension SearchViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        
+    }
+}
+
+extension SearchViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        //
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        //
+    }
+    
+    
+}
+
+//MARK: - UITableViewDataSource, UITableViewDelegate
+extension SearchViewController: UITableViewDataSource, UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sectionsTitles.count
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.width, height: 60))
+        
+        let label = UILabel(frame: CGRect(x: 15, y: 0, width: tableView.frame.width - 30, height: 20))
+        label.text = sectionsTitles[section]
+        label.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        label.textColor = UIColor.white
+        headerView.addSubview(label)
+        
+        return headerView
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: SearchCell.identifier, for: indexPath) as? SearchCell else {
+            fatalError("Error")
+        }
+        cell.delegate = self
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        //Play song, open album etc
+    }
+    
+}
+
+//MARK: - SearchCellDelegate
+extension SearchViewController: SearchCellDelegate {
+    func moreActionsButtonTapped(_ cell: SearchCell) {
+        print("More actions")
+    }
+    
+    
 }
