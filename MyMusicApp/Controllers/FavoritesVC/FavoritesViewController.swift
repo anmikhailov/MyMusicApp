628633f6a20477024293c5e0ee70b3cc5a3b96d3
Sergey Medvedev <devgod@mail.ru>
2023-06-18 12:13:20 +0300

    start FavoriteVC

--- MyMusicApp/Controllers/FavoritesVC/FavoritesViewController.swift
+++ MyMusicApp/Controllers/FavoritesVC/FavoritesViewController.swift
@@ -8,9 +8,30 @@
 import UIKit
 
 class FavoritesViewController: UIViewController {
-
+    enum Constans {
+        static let background = UIColor(named: "Background")
+    }
+    // MARK: - let/var
+    let favoriteView = FavoriteView()
+    // MARK: - lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        setupViews()
+        setConstrains()
+    }
+}
+extension FavoritesViewController {
+    // MARK: - setConstrains
+    private func setupViews() {
+        view.backgroundColor = Constans.background
+        
+        view.addSubview(favoriteView) 
+    }
+    // MARK: - setConstrains
+    private func setConstrains() {
+        NSLayoutConstraint.activate([
+            
+        ])
     }
 }
