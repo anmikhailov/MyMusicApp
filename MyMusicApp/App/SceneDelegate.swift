227d3d9eb47dc331732640a6ca022e1b837c3267
AndreyMikhailov <mands315@gmail.com>
2023-06-28 11:09:27 +0300

    New structure of project

--- MyMusicApp/App/SceneDelegate.swift
+++ MyMusicApp/App/SceneDelegate.swift
@@ -14,11 +14,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
-        let tabBarController = TabBarController()
-        
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = tabBarController
+        window?.rootViewController = UIViewController()
         window?.makeKeyAndVisible()
     }
 
